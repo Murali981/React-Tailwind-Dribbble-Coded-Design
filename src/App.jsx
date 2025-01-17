f0ccd4fa8eea702ae46cab1687f8f81b5200134a
Murali981 <muraliju981@gmail.com>
2025-01-17 20:27:35 +0530

    Completed till pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import ButtonGradient from "./assets/svg/ButtonGradient";
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
+import Colloboration from "./components/Colloboration";
+import Services from "./components/Services";
 
 const App = () => {
   return (
@@ -10,6 +13,9 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
+        <Colloboration />
+        <Services />
       </div>
       <ButtonGradient />
     </React.Fragment>
